9c1a06d792fb167e9ad401100cac1b5576e2797e
nitipon <nitipon.apaisri@gmail.com>
2024-09-27 13:19:55 +0700

    Refactor global styles and add dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-black`}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     <div className="p-4">
                         <SwitchTheme />
                     </div>
