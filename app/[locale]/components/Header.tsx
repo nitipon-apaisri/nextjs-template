a9bd5dd7a0ca5cb773f5b440314b59b2af80849b
nitipon <nitipon.apaisri@gmail.com>
2024-09-29 21:58:44 +0700

    Refactor Header component to update import paths and improve layout

--- app/[locale]/components/Header.tsx
+++ app/[locale]/components/Header.tsx
@@ -1,7 +1,10 @@
 import SwitchTheme from "@components/button/SwitchThemeButton";
 export default function Header() {
     return (
-        <header className="p-4">
+        <header className="flex justify-between items-center p-10">
+            <div className="min-w-60">
+                <div className="w-12 aspect-square bg-gray-500 dark:bg-gray-50 rounded-lg"></div>
+            </div>
             <SwitchTheme />
         </header>
     );
