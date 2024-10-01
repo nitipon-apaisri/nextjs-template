6d030b1cdfe1ff0132325efd6611d28094be1d95
nitipon <nitipon.apaisri@gmail.com>
2024-10-01 15:57:05 +0700

    Refactor layout to update body background color

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
 
     return (
         <html lang={locale}>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-black`}>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#121212]`}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     <Header />
                     <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
